5dc6929d769c2ea0d36645c018db50c1dcc22fb5
vdegenne <vdegenne@gmail.com>
2022-03-14 12:03:25 +0100

    t for text

--- content.js
+++ content.js
@@ -2,26 +2,36 @@ function getButton (content) {
   return [...document.querySelectorAll('button')].filter(b => b.innerText === content).pop()
 }
 
-window.addEventListener('keypress', function (event) {
-  if (event.key === 't') {
+window.addEventListener('keydown', function (event) {
+  if (event.altKey && (event.key === 't' || event.key === 'T')) {
+    event.stopPropagation()
+    event.preventDefault()
+    const button = [...document.querySelectorAll('button')].filter(b => b.innerText === 'Text')[0]
+    if (button) {
+      button.click()
+    }
+    return
+  }
+
+  if (event.key === 't' || event.key === 'T') {
     const button = getButton('Translate')
     if (button) {
       button.click()
     }
   }
-  if (event.key === 's') {
+  if (event.key === 's' || event.key === 'S') {
     const button = getButton('Listen')
     if (button) {
       button.click()
     }
   }
-  if(event.key === 'c') {
+  if(event.key === 'c' || event.key === 'C') {
     const button = getButton('Copy text')
     if (button) {
       button.click()
     }
   }
-  if(event.key === 'a') {
+  if(event.key === 'a' || event.key === 'A') {
     const query = document.querySelector('h2').innerText.replace(/\"/g, '').trim()
     if (query) {
       this.window.open(`http://www.google.com/search?q=${encodeURIComponent(query)}&tbm=isch`, '_blank')
@@ -29,14 +39,14 @@ window.addEventListener('keypress', function (event) {
   }
 
 
-  if(event.key === 'g') {
+  if(event.key === 'g' || event.key === 'G') {
     // Get the selected text
     const query = document.querySelector('h2').innerText.replace(/\"/g, '').trim()
     if (query) {
       this.window.open(`https://jisho.org/search/${encodeURIComponent(query)}`, '_blank')
     }
   }
-  if(event.key === 'h') {
+  if(event.key === 'h' || event.key === 'H') {
     // Get the selected text
     const query = document.querySelector('h2').innerText.replace(/\"/g, '').trim()
     if (query) {
